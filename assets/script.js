5bb7d2cbd0ea04dac9ef128a6afb32a414175ad4
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-01-20 06:24:09 -0500

    Minor styling adjustments to color, restructuring some improper HTML ordering

--- assets/script.js
+++
@@ -1,7 +0,0 @@
-// $("#linkedInIcon").on("click", function (event) {
-//     event.preventDefault();
-//     event.stopPropagation();
-//     event.isPropagationStopped()
-//     window.open("www.linkedin.com/in/chad-kaplan-599186198", "_blank")
-//     console.log("Clicked LinkedIn");
-// })
