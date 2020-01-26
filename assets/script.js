d09ec75b8be504487eb1764341079a0111038dbb
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-01-25 23:24:54 -0500

    Setup placeholders for projects

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,7 @@
 $("#contactInfo").on("submit", (event) => {
 event.preventDefault();
-const nameInput = document.getElementById("nameInput").value;
-const emailInput = document.getElementById("emailInput").value;
-const messageInput = document.getElementById("messageInput").value;
+const nameInput = document.getElementById("nameInput").value.trim();
+const emailInput = document.getElementById("emailInput").value.trim();
+const messageInput = document.getElementById("messageInput").value.trim();
 console.log(nameInput, emailInput, messageInput)
   })
