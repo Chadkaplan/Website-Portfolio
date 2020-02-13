b28fd4f50d8cc3755d67bcd607ab4767825b0e0a
Chad Kaplan <chadkaplan@knights.ucf.edu>
2020-02-12 23:45:00 -0500

    Comment out unused javascript

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,10 @@
-$("#contactInfo").on("submit", (event) => {
-event.preventDefault();
-const nameInput = document.getElementById("nameInput").value.trim();
-const emailInput = document.getElementById("emailInput").value.trim();
-const messageInput = document.getElementById("messageInput").value.trim();
-console.log(nameInput, emailInput, messageInput)
-  })
+// $("#contactInfo").on("submit", (event) => {
+// event.preventDefault();
+// const nameInput = document.getElementById("nameInput").value.trim();
+// const emailInput = document.getElementById("emailInput").value.trim();
+// const messageInput = document.getElementById("messageInput").value.trim();
+// console.log(nameInput, emailInput, messageInput)
+//   })
   // $("#openEmail").on("submit", (event) => {
   //   event.preventDefault();
   //   window.location.href = "mailto:chadkaplandev@gmail.com?subject=Subject&body=message%20goes%20here";
